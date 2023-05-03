9d461dd7a569d348377fed5b7927141d3a714650
walid habbach <79254928+walidhabbach@users.noreply.github.com>
2023-05-03 01:25:33 +0100

    Update

--- react_/src/Components/Pages/Car.js
+++ react_/src/Components/Pages/Car.js
@@ -1,8 +1,8 @@
-import { importAttribute } from '@babel/types';
+
 import React from 'react';
 import CarCard from '../Cards/CarCard';
 import { RangeInput, BrandSelect, FuelSelect, SearchBox } from '../Cards/SearchCard'
-
+import data from "../../data/brands_modelsAll.json"
 export default function Car() {
   const car = {
     make: 'Tesla',
@@ -26,10 +26,24 @@ export default function Car() {
         <div className="w-full md:w-2/3 py-4 px-6">
           <h1 className="text-center font-bold text-xl mb-4">Car Details</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            <CarCard image={car.image} title={car.make} details={car.model} />
-            <CarCard image={car.image} title={car.make} details={car.model} />
-            <CarCard image={car.image} title={car.make} details={car.model} />
-            <CarCard image={car.image} title={car.make} details={car.model} />
+            {Object.keys(data).map(brand =>
+              data[brand].map(model => (
+                <CarCard
+                  key={model.model}
+                  image={model.images[0]}
+                  brand={brand}
+                  model={model.model}
+                  price="500"
+                  year="{model.details['Start of production ']}"
+                  fuel="{model.details['Fuel Type ']}"
+                />
+              ))
+            )}
+
+
+
+
+
           </div>
         </div>
       </div>
